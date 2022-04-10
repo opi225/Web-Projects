b8b7cd541d85237e9db13bfbcc306d5315d84cef
opi225 <61334831+opi225@users.noreply.github.com>
2022-04-09 20:01:44 -0400

    Add files via upload

---
+++ School Projects/Hospital Search App/src/classes.js
@@ -0,0 +1,14 @@
+class Hospital {
+    constructor(name, lat, long, add, state, web, status, heli){
+        this.name = name;
+        this.lat = lat;
+        this.long = long;
+        this.add = add;
+        this.state = state;
+        this.web = web;
+        this.status = status;
+        this.heli = heli;
+    }
+}
+
+export{Hospital}
